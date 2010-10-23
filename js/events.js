365a5adf1220c92cc6a117575b9d9f23064cc31c
Carlos Correa <carloscorrea137@gmail.com>
2010-10-23 03:26:08 -0500

    build list of cards

--- js/events.js
+++ js/events.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    cards = [];
     var nm = new NotificationManager($('body'));
     $('#question').keypress(function(e) {
         if(e.keyCode == '13') {
@@ -8,8 +9,9 @@ $(document).ready(function() {
     $('#answer').keypress(function(e) {
         if(e.keyCode == '13') {
             $('#question').focus();
+            cards.push([$('#question').val(), $('#answer').val()]);
             nm.show("hello");
+            $('#question').add('#answer').text("");
         }
     });
-
 });
